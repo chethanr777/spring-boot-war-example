e20c21029ff715e57c3d6cef956ba55a6316a2a0
chethanr777 <crhogenakal@gmail.com>
2024-08-11 15:08:25 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World! War file launched in Tomcat server Successfully";
+        return "Hello World! Jenkinsfile.declarative!!!!";
     }
 
 }
