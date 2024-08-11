6713815b07b9a29c27cde4684d5a2b307ce81565
chethanr777 <crhogenakal@gmail.com>
2024-08-11 14:54:35 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,44 +1,31 @@
-pipeline {
-    agent any
-    tools {
-        maven 'maven'
+node {
+    def TOMCAT_USER = 'ubuntu'
+    def TOMCAT_HOST = '18.60.93.17'
+    def TOMCAT_PATH = '/home/ubuntu/tomcat/webapps/'
+
+    stage('Checkout') {
+        git branch: 'master', credentialsId: 'github', url: 'https://github.com/chethanr777/spring-boot-war-example.git'
     }
-    environment {
-        TOMCAT_USER = 'ubuntu'
-        TOMCAT_HOST = '98.130.54.193'
-        TOMCAT_PATH = '/home/ubuntu/tomcat/webapps'
+
+    stage('Build') {
+        try {
+            sh 'mvn clean package'
+            archiveArtifacts artifacts: '**/target/*.war'
+            echo "Archiving the Artifacts"
+        } catch (Exception e) {
+            error "Build failed: ${e.message}"
+        }
     }
-    stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'master', credentialsId: 'github', url: 'https://github.com/chethanr777/spring-boot-war-example.git'
+
+    stage('Deploy') {
+        try {
+            sshagent(['Master-tomcat']) {
+                sh "rsync -avz -e 'ssh -o StrictHostKeyChecking=no' --delete target/*.war ${TOMCAT_USER}@${TOMCAT_HOST}:${TOMCAT_PATH}"
             }
-        }
-        stage('Build') {
-            steps {
-                sh 'mvn clean package'
-            }
-            post {
-                success {
-                    echo "Archiving the Artifacts"
-                    archiveArtifacts artifacts: '**/target/*.war'
-                }
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sshagent(['Master-tomcat']) {
-                    sh "rsync -avz -e 'ssh -o StrictHostKeyChecking=no' --delete target/*.war ${TOMCAT_USER}@${TOMCAT_HOST}:${TOMCAT_PATH}"
-                }
-            }
-            post {
-                failure {
-                    echo "Deployment failed!"
-                }
-                success {
-                    echo "Deployment succeeded!"
-                }
-            }
+            echo "Deployment succeeded!"
+        } catch (Exception e) {
+            echo "Deployment failed: ${e.message}"
+            error "Deployment failed!"
         }
     }
 }
